d6c9aa2c86816f660932a0802f9705ea970ef054
Muhammed Alsabban <muals21@student.sdu.dk>
2024-02-01 14:40:32 +0100

    The project is initialized. Dependencies are added

---
+++ src/popup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {render } from "react-dom";
+
+function Popup () {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <p>This is a popup</p>
+        </div>
+    );
+
+}
+
+render(<Popup />, document.getElementById('react-target'));
