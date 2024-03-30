c8b575405140487b9818211b530fecda4acc7151
Muhammed Alsabban <muals21@student.sdu.dk>
2024-03-30 22:44:52 +0100

    done with manipulating lines

--- src/components/visual.jsx
+++ src/components/visual.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Visual(){
     return (
         <p>
-           Visual...
+            You will experience the following: <br />
+            1. Word movement <br />
+            2. The mirror image of a letter will be displayed <br />
+            3. Rearrange letters within a word  <br />
         </p>
     );
 }
