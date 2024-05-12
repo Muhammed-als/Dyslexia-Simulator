c86db997250cbc78dbfef10758ea41707247e6ce
Muhammed Alsabban <muals21@student.sdu.dk>
2024-05-12 20:42:39 +0200

    change text description

--- src/components/surface.jsx
+++ src/components/surface.jsx
@@ -3,8 +3,8 @@ function Surface(){
     return(
         <p>
             You will experience the following: <br />
-            1. Changing in font settings. <br />
-            2. Blur effect on text. 
+            1. Changing in font style. <br />
+            2. Blur effect on text. <br />
         </p>
     );
 }
